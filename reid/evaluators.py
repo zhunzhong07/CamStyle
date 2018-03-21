7ce2271177b416fc866fe5893016ce4e61c0b861
zhunzhong07 <zhun.zhong@foxmail.com>
2018-03-21 17:59:39 +1100

    add reranking

--- reid/evaluators.py
+++ reid/evaluators.py
@@ -101,13 +101,87 @@ def evaluate_all(distmat, query=None, gallery=None,
     return cmc_scores['market1501'][0]
 
 
+def reranking(query_features, gallery_features, query=None, gallery=None, k1=20, k2=6, lamda_value=0.3):
+        x = torch.cat([query_features[f].unsqueeze(0) for f, _, _ in query], 0)
+        y = torch.cat([gallery_features[f].unsqueeze(0) for f, _, _ in gallery], 0)
+        feat = torch.cat((x, y))
+        query_num, all_num = x.size(0), feat.size(0)
+        feat = feat.view(all_num, -1)
+
+        dist = torch.pow(feat, 2).sum(dim=1, keepdim=True).expand(all_num, all_num)
+        dist = dist + dist.t()
+        dist.addmm_(1, -2, feat, feat.t())
+
+        original_dist = dist.numpy()
+        all_num = original_dist.shape[0]
+        original_dist = np.transpose(original_dist / np.max(original_dist, axis=0))
+        V = np.zeros_like(original_dist).astype(np.float16)
+        initial_rank = np.argsort(original_dist).astype(np.int32)
+
+        print('starting re_ranking')
+        for i in range(all_num):
+            # k-reciprocal neighbors
+            forward_k_neigh_index = initial_rank[i, :k1 + 1]
+            backward_k_neigh_index = initial_rank[forward_k_neigh_index, :k1 + 1]
+            fi = np.where(backward_k_neigh_index == i)[0]
+            k_reciprocal_index = forward_k_neigh_index[fi]
+            k_reciprocal_expansion_index = k_reciprocal_index
+            for j in range(len(k_reciprocal_index)):
+                candidate = k_reciprocal_index[j]
+                candidate_forward_k_neigh_index = initial_rank[candidate, :int(np.around(k1 / 2)) + 1]
+                candidate_backward_k_neigh_index = initial_rank[candidate_forward_k_neigh_index,
+                                                   :int(np.around(k1 / 2)) + 1]
+                fi_candidate = np.where(candidate_backward_k_neigh_index == candidate)[0]
+                candidate_k_reciprocal_index = candidate_forward_k_neigh_index[fi_candidate]
+                if len(np.intersect1d(candidate_k_reciprocal_index, k_reciprocal_index)) > 2 / 3 * len(
+                        candidate_k_reciprocal_index):
+                    k_reciprocal_expansion_index = np.append(k_reciprocal_expansion_index, candidate_k_reciprocal_index)
+
+            k_reciprocal_expansion_index = np.unique(k_reciprocal_expansion_index)
+            weight = np.exp(-original_dist[i, k_reciprocal_expansion_index])
+            V[i, k_reciprocal_expansion_index] = weight / np.sum(weight)
+        original_dist = original_dist[:query_num, ]
+        if k2 != 1:
+            V_qe = np.zeros_like(V, dtype=np.float16)
+            for i in range(all_num):
+                V_qe[i, :] = np.mean(V[initial_rank[i, :k2], :], axis=0)
+            V = V_qe
+            del V_qe
+        del initial_rank
+        invIndex = []
+        for i in range(all_num):
+            invIndex.append(np.where(V[:, i] != 0)[0])
+
+        jaccard_dist = np.zeros_like(original_dist, dtype=np.float16)
+
+        for i in range(query_num):
+            temp_min = np.zeros(shape=[1, all_num], dtype=np.float16)
+            indNonZero = np.where(V[i, :] != 0)[0]
+            indImages = []
+            indImages = [invIndex[ind] for ind in indNonZero]
+            for j in range(len(indNonZero)):
+                temp_min[0, indImages[j]] = temp_min[0, indImages[j]] + np.minimum(V[i, indNonZero[j]],
+                                                                                   V[indImages[j], indNonZero[j]])
+            jaccard_dist[i] = 1 - temp_min / (2 - temp_min)
+
+        final_dist = jaccard_dist * (1 - lamda_value) + original_dist * lamda_value
+        del original_dist
+        del V
+        del jaccard_dist
+        final_dist = final_dist[:query_num, query_num:]
+        return final_dist
+
+
 class Evaluator(object):
     def __init__(self, model):
         super(Evaluator, self).__init__()
         self.model = model
 
-    def evaluate(self, query_loader, gallery_loader, query, gallery, output_feature=None):
+    def evaluate(self, query_loader, gallery_loader, query, gallery, output_feature=None, rerank=False):
         query_features, _ = extract_features(self.model, query_loader, 1, output_feature)
         gallery_features, _ = extract_features(self.model, gallery_loader, 1, output_feature)
-        distmat = pairwise_distance(query_features, gallery_features, query, gallery)
+        if rerank:
+            distmat = reranking(query_features, gallery_features, query, gallery)
+        else:
+            distmat = pairwise_distance(query_features, gallery_features, query, gallery)
         return evaluate_all(distmat, query=query, gallery=gallery)
